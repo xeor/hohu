383cda9776636056da1c77589ac75f14e272daed
Lars Solberg <lars.solberg@gmail.com>
2016-12-31 01:02:16 +0100

    Made href ports configurable if you are going through several proxies. There is no way for the server to know which port eg docker is using and the browser access the service on. Links generated by django-rest-framework was wrong

---
+++ api/data/src/lib/middleware.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+
+class SetBaseEnv(object):
+    """
+    Figure out which port we are on if we are running and set it.
+
+    So that the links will be correct.
+    Not sure if we need this always...
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if os.environ.get('HTTP_PORT'):
+            request.META['HTTP_HOST'] = '{}:{}'.format(request.META['HTTP_HOST'], os.environ['HTTP_PORT'])
+
+        response = self.get_response(request)
+        return response
--- api/data/src/settings.py
+++ api/data/src/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'lib.middleware.SetBaseEnv',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
