ab5edb47eb50fe2434432d76d5599f5a2f168f57
Lars Solberg <lars.solberg@gmail.com>
2017-01-07 02:02:27 +0100

    Cleanups

--- www/data/src/app/user/user.service.ts
+++ www/data/src/app/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
-import { API_BASE_URL } from '../services/constants';
+import { BASE_URL } from '../services/constants';
 import { RequestBase } from '../services/request-base';
 
 @Injectable()
@@ -12,7 +12,7 @@ export class UserService extends RequestBase {
   }
 
   logout(): Observable<string> {
-    return this.http.get(`${API_BASE_URL}/logout`, this.optionsNoPre)
+    return this.http.get(`${BASE_URL}/logout`, this.optionsNoPre)
       .map(res => res.text());
   }
 }
